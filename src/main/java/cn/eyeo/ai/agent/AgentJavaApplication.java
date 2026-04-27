54a3116e952f7d6d4aab1951740bd45951a78145
dorian <daoyuan0626@gmail.com>
2026-04-27 23:14:57 +0800

    feat: init project

---
+++ src/main/java/cn/eyeo/ai/agent/AgentJavaApplication.java
@@ -0,0 +1,19 @@
+package cn.eyeo.ai.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Java Agent 启动类
+ *
+ * @author dorian
+ * @date 2026/04/27
+ */
+@SpringBootApplication
+public class AgentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/eyeo/ai/agent/AgentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.eyeo.ai.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
